670abe81aae06404b879f48262fc032a6c5b5130
Diego Torres <diegopaivatorres@gmail.com>
2026-05-24 03:01:29 -0400

    Update SDK versions and migrate to ViewBinding
    - Update `compileSdk` and `targetSdk` to 34 and `minSdk` to 31.
    - Migrate from deprecated Kotlin Android Extensions (synthetics) to ViewBinding.
    - Upgrade Gradle to 9.4.1, AGP to 9.2.1, and Kotlin to 2.2.10.
    - Update dependencies to modern versions, including AppCompat, Lifecycle, and Play Services Ads.
    - Replace `jcenter()` with `mavenCentral()` and configure `namespace` in `build.gradle`.
    - Refactor `MainFragment` to use `ViewBinding`, `ViewModelProvider`, and `onViewCreated`.

--- app/src/main/java/com/diegotorresbr/dzimaperiodicaparafrao/ui/main/MainFragment.kt
+++ app/src/main/java/com/diegotorresbr/dzimaperiodicaparafrao/ui/main/MainFragment.kt
@@ -1,6 +1,6 @@
 package com.diegotorresbr.dzimaperiodicaparafrao.ui.main
 
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
 import android.util.Rational
@@ -8,11 +8,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 import com.diegotorresbr.dzimaperiodicaparafrao.R
+import com.diegotorresbr.dzimaperiodicaparafrao.databinding.FragmentoBinding
 import com.google.android.gms.ads.AdListener
-import kotlinx.android.synthetic.main.fragmento.*
+import com.google.android.gms.ads.LoadAdError
 import kotlin.math.pow
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.AdRequest
@@ -26,40 +25,43 @@ class MainFragment : Fragment() {
     }
 
     private lateinit var viewModel: MainViewModel
+    private var _binding: FragmentoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragmento, container, false)
+        _binding = FragmentoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
-        MobileAds.initialize(getActivity()){}
+        MobileAds.initialize(requireContext()){}
         val adRequest = AdRequest.Builder().build()
-        adView.loadAd(adRequest)
+        binding.adView.loadAd(adRequest)
 
-        adView.adListener = object : AdListener(){
+        binding.adView.adListener = object : AdListener(){
             override fun onAdLoaded() {
                 Log.i("ADS", "Carregou")
                 super.onAdLoaded()
 
             }
 
-            override fun onAdFailedToLoad(p0: Int) {
+            override fun onAdFailedToLoad(p0: LoadAdError) {
                 Log.e("ADS", "Falhou"+p0.toString())
                 super.onAdFailedToLoad(p0)
             }
         }
 
-        var parte_inteira= p_inteira.text
-        var parte_nao_repete = p_n_repete.text
-        var dizima = p_repete.text
+        var parte_inteira= binding.pInteira.text
+        var parte_nao_repete = binding.pNRepete.text
+        var dizima = binding.pRepete.text
 
-        botao.setOnClickListener(View.OnClickListener {
+        binding.botao.setOnClickListener(View.OnClickListener {
             var n1 = 0.0
             var n2 = 0.0
             var n3 = dizima.toString().toDouble()
@@ -82,8 +84,8 @@ class MainFragment : Fragment() {
 
                 var fracao  = Rational(numerador.toInt(), denomindadoro*parte_fracao)
 
-                texto_numerador.text = fracao.numerator.toString()
-                texto_denominador.text = fracao.denominator.toString()
+                binding.textoNumerador.text = fracao.numerator.toString()
+                binding.textoDenominador.text = fracao.denominator.toString()
 
             }else{
 
@@ -92,12 +94,17 @@ class MainFragment : Fragment() {
                 var denomindador_ = denominador(dizima.toString()).toInt()
                 var numerador = (denomindador_*n1)+n3
                 var fracao  = Rational(numerador.toInt(), denomindador_)
-                texto_numerador.text = fracao.numerator.toString()
-                texto_denominador.text = fracao.denominator.toString()
+                binding.textoNumerador.text = fracao.numerator.toString()
+                binding.textoDenominador.text = fracao.denominator.toString()
             }
-            entrada.text = numero.toString()
+            binding.entrada.text = numero.toString()
 
         })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     fun denominador(numero:String): String {
